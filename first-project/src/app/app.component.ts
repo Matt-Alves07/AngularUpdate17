86f712fea9ddaff873c54584d17a4c793ba63e55
Matheus Alves <mathalves07.winstore@outlook.com>
2024-01-14 21:04:53 -0300

    Created the first component using Angular 17
    resolves #2

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { TemplateComponent } from './components/template/template.component';
+import { NewComponentComponent } from './components/new-component/new-component.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponentComponent, TemplateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
